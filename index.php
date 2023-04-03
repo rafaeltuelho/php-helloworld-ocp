9cc69cffb73aabc076783acddee7a63f2fb9d3af
Rafael T. C. Soares (A.K.A Tuelho) <rafaeltuelho@users.noreply.github.com>
2023-04-03 17:28:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-echo "<h1>Hello from Openshift!!!</h1> ";
-echo "<h2><b>POD IP Address: "
-echo $_SERVER['SERVER_ADDR']
-echo "</b></h2>"
+echo "<h1>Hello from Openshift!</h1> ";
+echo "<h2><b>POD internal IP Address: ".$_SERVER['SERVER_ADDR']."</b></h2>";
 ?>
